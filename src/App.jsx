18ad9c806d43d963f498ca823bc7e0907568667f
rizzo <samuelrizzo5@gmail.com>
2023-02-17 21:59:20 -0300

    quiz finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+//react
+import { useContext, useEffect } from 'react'
+import { QuizContext } from './context/quiz'
+
+//componentes
+import Question from './components/Question'
+import Welcome from './components/Welcome'
+import GameOver from './components/GameOver'
+
+//css
 import './App.css'
 
-import Welcome from './components/Welcome'
-
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS"})
+
+  }, [])
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Welcome />
+      
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+
     </div>
   )
 }

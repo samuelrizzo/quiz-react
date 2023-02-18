11529023324216720d12b0f4fbbc6aa97ed463e0
rizzo <samuelrizzo5@gmail.com>
2023-02-17 22:11:04 -0300

    Adicionadas perguntas criadas pelo ChatGPT

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,35 +1,80 @@
-const data = [
-    {
-      question: "O que é Vanilla JavaScript?",
-      options: [
-        "JavaScript puro",
-        "Uma biblioteca JavaScript",
-        "Um framework JavaScript",
-        "Um compilador de JavaScript",
-      ],
-      answer: "JavaScript puro",
-    },
-    {
-      question: "Com qual instrução declaramos uma constante em JavaScript?",
-      options: ["const", "let", "var", "define"],
-      answer: "const",
-    },
-    {
-      question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-      options: ["string", "number", "boolean", "float"],
-      answer: "float",
-    },
-    {
-      question: "Qual dos métodos a seguir seleciona um elemento?",
-      options: ["querySelector", "parseInt", "sort", "reduce"],
-      answer: "querySelector",
-    },
-    {
-      question:
-        "Qual destas propriedades da a quantidade de elementos de um array?",
-      options: ["qty", "length", "items", "index"],
-      answer: "length",
-    },
+const data = [  {    question: "O que é Vanilla JavaScript?",    options: [      "JavaScript puro",      "Uma biblioteca JavaScript",      "Um framework JavaScript",      "Um compilador de JavaScript",    ],
+    answer: "JavaScript puro",
+  },
+  {
+    question: "Com qual instrução declaramos uma constante em JavaScript?",
+    options: ["const", "let", "var", "define"],
+    answer: "const",
+  },
+  {
+    question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+    options: ["string", "number", "boolean", "float"],
+    answer: "float",
+  },
+  {
+    question: "Qual dos métodos a seguir seleciona um elemento?",
+    options: ["querySelector", "parseInt", "sort", "reduce"],
+    answer: "querySelector",
+  },
+  {
+    question:
+      "Qual destas propriedades da a quantidade de elementos de um array?",
+    options: ["qty", "length", "items", "index"],
+    answer: "length",
+  },
+  {
+    question: "O que é o DOM em JavaScript?",
+    options: [
+      "Uma representação do estado atual da página",
+      "Uma linguagem de programação",
+      "Uma biblioteca JavaScript",
+      "Um modelo de objeto do documento",
+    ],
+    answer: "Um modelo de objeto do documento",
+  },
+  {
+    question: "Qual é o operador de igualdade estrita em JavaScript?",
+    options: ["===", "==", "=", "!="],
+    answer: "===",
+  },
+  {
+    question: "Qual dos operadores a seguir representa OR lógico?",
+    options: ["||", "&&", "!", "&"],
+    answer: "||",
+  },
+  {
+    question: "Qual é a diferença entre null e undefined em JavaScript?",
+    options: [
+      "Null representa a ausência intencional de um valor, enquanto undefined representa uma variável que não foi atribuída a um valor",
+      "Undefined representa a ausência intencional de um valor, enquanto null representa uma variável que não foi atribuída a um valor",
+      "Null é um valor primitivo, enquanto undefined é um objeto",
+      "Undefined é um valor primitivo, enquanto null é um objeto",
+    ],
+    answer:
+      "Null representa a ausência intencional de um valor, enquanto undefined representa uma variável que não foi atribuída a um valor",
+  },
+  {
+    question:
+      "Qual dos operadores a seguir é utilizado para definir o tipo de uma variável em JavaScript?",
+    options: [":", "=", "===", "typeof"],
+    answer: "typeof",
+  },
+  {
+    question:
+      "Qual é o método utilizado para adicionar um elemento ao final de um array em JavaScript?",
+    options: ["push()", "unshift()", "concat()", "slice()"],
+    answer: "push()",
+  },
+  {
+    question: "O que é hoisting em JavaScript?",
+    options: [
+      "Uma função em JavaScript que retorna um valor booleano",
+      "O processo de mover declarações de variáveis para o topo de seu escopo",
+      "Um erro de sintaxe em JavaScript",
+      "Uma técnica para definir escopos em JavaScript",
+    ],
+    answer: "O processo de mover declarações de variáveis para o topo de seu escopo",
+  },
   ];
   
   export default data;
